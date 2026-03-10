3919c1e5f921fa2c1d6223bbe0def77c526dbbd0
gus-p3 <sinnamexd3432@gmail.com>
2026-03-09 19:55:13 -0600

    Feat: Se agrego la seguridad del JWT asi como las colecciones, servicios de routes de un usuarios

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import { pedidosRoutes } from './rutas/pedidos.js'
+import { usuarioRoutes } from './rutas/usuarios.js'
+
 // Crear la aplicación Express
 const app = express()
 // Configurar middlewares
@@ -10,6 +12,8 @@ app.use(cors())
 app.use(bodyParser.json())
 // Configurar rutas
 pedidosRoutes(app)
+usuarioRoutes(app)
+
 // Ruta de prueba
 app.get('/', (req, res) => {
     res.send('Hola from Express!')
